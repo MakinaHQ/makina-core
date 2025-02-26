db00fa0b259c0391c7af761b78e89cbfd7c51e2b
clempetit <clementpetit.sg@gmail.com>
2025-02-26 11:08:16 +0100

    chore: improve machine fuzz tests

---
+++ test/integration/fuzz/machine/convertToShares.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {MockERC20} from "test/mocks/MockERC20.sol";
+import {MockPriceFeed} from "test/mocks/MockPriceFeed.sol";
+import {Machine} from "src/machine/Machine.sol";
+import {Constants} from "src/libraries/Constants.sol";
+
+import {Machine_Integration_Fuzz_Test} from "./Machine.t.sol";
+
+contract ConvertToShares_Integration_Fuzz_Test is Machine_Integration_Fuzz_Test {
+    MockERC20 public accountingToken;
+
+    Machine public machine;
+
+    function _fuzzTestSetupAfter(uint256 atDecimals) public {
+        atDecimals =
+            uint8(bound(atDecimals, Constants.MIN_ACCOUNTING_TOKEN_DECIMALS, Constants.MAX_ACCOUNTING_TOKEN_DECIMALS));
+
+        accountingToken = new MockERC20("Accounting Token", "ACT", atDecimals);
+
+        MockPriceFeed aPriceFeed1 = new MockPriceFeed(18, int256(1e18), block.timestamp);
+
+        vm.prank(dao);
+        oracleRegistry.setTokenFeedData(
+            address(accountingToken), address(aPriceFeed1), DEFAULT_PF_STALE_THRSHLD, address(0), 0
+        );
+
+        (machine,,) = _deployMachine(address(accountingToken), HUB_CALIBER_ACCOUNTING_TOKEN_POS_ID, bytes32(0));
+    }
+
+    function testFuzz_ConvertToShares(uint256 atDecimals, uint256 assets) public {
+        _fuzzTestSetupAfter(atDecimals);
+        assets = bound(assets, 0, 1e40);
+
+        // deposit assets into the machine
+        accountingToken.mint(address(this), assets);
+        accountingToken.approve(address(machine), assets);
+        machine.deposit(assets, address(this));
+
+        // should hold when no yield occurred
+        assertEq(machine.convertToShares(10 ** accountingToken.decimals()), 10 ** Constants.SHARE_TOKEN_DECIMALS);
+    }
+}
