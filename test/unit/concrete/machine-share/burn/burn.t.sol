557621f4ce01d5e11485f3ed723956117f82de2c
Clément Petit <33152614+clempetit@users.noreply.github.com>
2025-08-19 13:34:35 +0200

    feat: misc minor updates (#146)
    * feat: allow permissionless self-burn of machine shares
    
    * feat: minor updates in IFeeManager (#147)

--- test/unit/concrete/machine-share/burn/burn.t.sol
+++ test/unit/concrete/machine-share/burn/burn.t.sol
@@ -6,19 +6,35 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {MachineShare_Unit_Concrete_Test} from "../MachineShare.t.sol";
 
 contract Burn_Unit_Concrete_Test is MachineShare_Unit_Concrete_Test {
-    function test_RevertWhen_CallerNotMinter() public {
-        uint256 amount = 100;
-        deal(address(shareToken), address(this), amount, true);
+    address private user;
 
-        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(this)));
-        shareToken.burn(address(this), amount);
+    function setUp() public override {
+        MachineShare_Unit_Concrete_Test.setUp();
+        user = makeAddr("user");
     }
 
-    function test_Burn() public {
+    function test_Burn_From_RevertWhen_CallerNotOwner() public {
+        uint256 amount = 100;
+        deal(address(shareToken), user, amount, true);
+
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(this)));
+        shareToken.burn(user, amount);
+    }
+
+    function test_Burn_From() public {
+        uint256 amount = 100;
+        deal(address(shareToken), user, amount, true);
+
+        vm.prank(address(machine));
+        shareToken.burn(user, amount);
+        assertEq(shareToken.balanceOf(user), 0);
+        assertEq(shareToken.balanceOf(address(this)), 0);
+    }
+
+    function test_Burn_Self() public {
         uint256 amount = 100;
         deal(address(shareToken), address(this), amount, true);
 
-        vm.prank(address(machine));
         shareToken.burn(address(this), amount);
         assertEq(shareToken.balanceOf(address(this)), 0);
     }
